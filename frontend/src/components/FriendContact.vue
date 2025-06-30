bab51fa4874f853d201b4674167a4806a81191c2
Jonathan Eberle <jonathan.eberle@gmx.de>
2025-06-30 17:41:50 +0200

    styles and basic vue

---
+++ frontend/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+	<li>
+	  <h2>{{ friend.name }}</h2>
+	  <button @click="toggleDetails" class="btn">Show Details</button>
+	  <ul v-if="detailsAreVisible">
+		<li><strong>Phone:</strong> {{ friend.phone }}</li>
+		<li><strong>Email:</strong> {{ friend.email }}</li>
+	  </ul>
+	</li>
+  </template>
+
+  <script>
+  export default {
+	props: ['friend'], // Props definieren
+	data() {
+	  return {
+		detailsAreVisible: false
+	  }
+	},
+	methods: {
+	  toggleDetails() {
+		this.detailsAreVisible = !this.detailsAreVisible
+	  }
+	}
+  }
+  </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+import './assets/styles/index.css'
+import FriendContact from './components/FriendContact.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('friend-contact', FriendContact)
+app.mount('#app')
